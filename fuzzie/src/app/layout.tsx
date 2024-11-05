ae8a5ed156fd2bb9363847e64379625130207214
swastideep maharana <swastideep67maharana@gmail.com>
2024-11-05 22:07:08 +0530

    #adding the navbar

--- fuzzie/src/app/layout.tsx
+++ fuzzie/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { ThemeProvider } from "@/providers/theme-providers";
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -28,7 +29,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        
       </body>
     </html>
   );
