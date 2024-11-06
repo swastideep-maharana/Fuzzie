830d02140c92010480f50f19201016c6bb98d34f
swastideep maharana <swastideep67maharana@gmail.com>
2024-11-06 17:18:03 +0530

    #added the dashboard page and some setup for the project

--- fuzzie/src/app/page.tsx
+++ fuzzie/src/app/page.tsx
@@ -1,10 +1,50 @@
-import Navbar from '@/components/global/navbar'
-import Image from 'next/image'
+import { HeroParallax } from '@/components/global/connect-parallax';
+import { ContainerScroll } from '@/components/global/container-scroll-animation';
+import { InfiniteMovingCards } from '@/components/global/InfiniteMovingCards ';
+
+import Navbar from '@/components/global/navbar';
+import { Button } from '@/components/ui/button';
+import { clients, products } from '@/lib/constants';
 
 export default function Home() {
   return (
-    <main >
+    <main>
       <Navbar />
+      <section className="h-screen w-full bg-neutral-950 rounded-md overflow-visible relative flex flex-col items-center antialiased">
+        <div className="absolute inset-0 h-full w-full px-5 py-24 bg-[radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
+        
+        <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
+          <ContainerScroll
+            titleComponent={
+              <div className="flex items-center justify-center flex-col text-center">
+                <Button
+                  size="lg"
+                  className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-auto border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white transition-all duration-500 group"
+                  aria-label="Start for free today"
+                >
+                  <span className="bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600 font-sans group-hover:bg-gradient-to-r group-hover:from-black group-hover:to-black">
+                    Start For Free Today
+                  </span>
+                </Button>
+                
+                <h1 className="text-5xl md:text-8xl bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
+                  Automate Your Work With Fuzzie
+                </h1>
+              </div>
+            }
+          />
+        </div>
+      </section>
+
+      <InfiniteMovingCards
+        className="mt-[2rem] md:mt-[18rem]"
+        items={clients}
+        direction="right"
+        speed="slow"
+      />
+      <section>
+        <HeroParallax products={products}></HeroParallax>
+      </section>
     </main>
-  )
-} 
+  );
+}
