f52828140ec33ed29c53de9753ed506e165bcba2
swastideep maharana <swastideep67maharana@gmail.com>
2024-11-15 09:21:53 +0530

    # added settings page and components

---
+++ fuzzie/src/components/forms/profile-form.tsx
@@ -0,0 +1,54 @@
+'use client'
+
+import { Edit } from 'lucide-react'
+import {z} from 'zod'
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { EditUserProfileSchema } from '@/lib/types'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../ui/form'
+import { Input } from '../ui/input'
+
+type Props = {}
+
+const ProfileForm = (props: Props) => {
+    const [isLoading, setIsLoading] = useState(false)
+
+    const form =useForm<z.infer<typeof EditUserProfileSchema>>({
+        mode: "onChange",
+        resolver: zodResolver(EditUserProfileSchema),
+        defaultValues: {
+            name:"",
+            email: "",
+        },
+    })
+
+  return <Form {...form}>
+    <form 
+    className='flex flex-col gap-6'
+    onSubmit={() => {}}
+    >
+        <FormField 
+            disabled={isLoading}
+            control={form.control}
+            name={"name"}
+            render={({field}) => (
+                <FormItem>
+                    <FormLabel className='text-lg'>User full name</FormLabel>
+                    <FormControl>
+                        <Input 
+                        placeholder="Name"
+                        {...field}
+                        />
+                    </FormControl>
+                    <FormMessage />
+                </FormItem>
+            )}
+        />
+    </form>
+  </Form>
+    
+  
+}
+
+export default ProfileForm
